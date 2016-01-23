cace689fa7b7bf350bc263d649ae98fb168cefa4
Nooria <nooriashs@gmail.com>
2016-01-23 14:10:24 +0300

    ch

--- views/js/main.js
+++ views/js/main.js
@@ -447,11 +447,10 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    var container =  document.getElementsByClassName('randomPizzaContainer');
-    var dx = determineDx(container[0], size);
-    var newwidth = (container[0].offsetWidth + dx) + 'px';
-   for (var i = 0, len = container.length; i < len; i++) {
-      container[i].style.width = newwidth;
+    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
+      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
+      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
     }
   };
 
