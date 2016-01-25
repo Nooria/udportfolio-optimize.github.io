22265cd7369b9ac53a76d9f0c6e54c184fbe230e
Nooria <nooriashs@gmail.com>
2016-01-25 16:52:03 +0300

    ch

--- views/js/main.js
+++ views/js/main.js
@@ -451,7 +451,7 @@ var resizePizzas = function(size) {
     var dx = determineDx(container[0], size);
     var newwidth = (container[0].offsetWidth + dx) + 'px';
     for (var i = 0, len = container.length; i < len; i++) {
-       container[i].style.width = newwidth;
+      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
     }
   };
 
